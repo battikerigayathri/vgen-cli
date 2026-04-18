a94f09c632880ad2439d5db5d73c1b02d7b9592e
Praveen Vuddagiri <vuddagiripraveen@gmail.com>
2026-04-18 23:13:43 +0530

    inti

--- target/debug/build/serde_json-9710288fcc24bc28/build_script_build-9710288fcc24bc28.d
+++
@@ -1,5 +0,0 @@
-C:\Users\vithi\Downloads\Archive 3\target\debug\build\serde_json-9710288fcc24bc28\build_script_build-9710288fcc24bc28.d: C:\Users\vithi\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\serde_json-1.0.149\build.rs
-
-C:\Users\vithi\Downloads\Archive 3\target\debug\build\serde_json-9710288fcc24bc28\build_script_build-9710288fcc24bc28.exe: C:\Users\vithi\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\serde_json-1.0.149\build.rs
-
-C:\Users\vithi\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\serde_json-1.0.149\build.rs:
--- target/debug/build/windows_x86_64_msvc-38540bf3181b7a31/build_script_build-38540bf3181b7a31.d
+++
@@ -1,5 +0,0 @@
-C:\Users\vithi\Downloads\Archive 3\target\debug\build\windows_x86_64_msvc-38540bf3181b7a31\build_script_build-38540bf3181b7a31.d: C:\Users\vithi\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows_x86_64_msvc-0.52.6\build.rs
-
-C:\Users\vithi\Downloads\Archive 3\target\debug\build\windows_x86_64_msvc-38540bf3181b7a31\build_script_build-38540bf3181b7a31.exe: C:\Users\vithi\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows_x86_64_msvc-0.52.6\build.rs
-
-C:\Users\vithi\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\windows_x86_64_msvc-0.52.6\build.rs:
